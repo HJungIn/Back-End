7237cd46d5041ac00bb4353a708a52823a3ee777
HJungIn <gjwjddls96@naver.com>
2020-11-08 22:03:08 +0900

    Initial commit

---
+++ src/main/java/com/project/gonggus/GonggusApplication.java
@@ -0,0 +1,13 @@
+package com.project.gonggus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonggusApplication.class, args);
+	}
+
+}
