40273a5b0df99107cc65bfe53eed414934f5b8e3
HJungIn <gjwjddls96@naver.com>
2020-11-18 15:19:05 +0900

    change @Restcontoller

--- src/main/java/com/project/gonggus/controller/PostController.java
+++ src/main/java/com/project/gonggus/controller/PostController.java
@@ -4,67 +4,55 @@ import com.project.gonggus.domain.post.Post;
 import com.project.gonggus.domain.post.PostService;
 import com.project.gonggus.domain.user.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+
+@RestController
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
-    @RequestMapping("/{category}")
-    public String categoryPost(Model model, @PathVariable("category") String category){
-        List<Post> categoryPosts = postService.getCategoryPosts(category);
-        model.addAttribute("categoryPosts",categoryPosts);
-        return "categorypost";
+    @GetMapping("/{category}")
+    public List<Post> categoryPost(@PathVariable("category") String category){
+        return postService.getCategoryPosts(category);
+    }
+
+    @GetMapping("/searchpost")
+    public List<Post> searchPost(@RequestParam("search") String searchTitle){
+        return postService.getSearchPosts(searchTitle);
     }
 
 
-    @RequestMapping("/searchpost")
-    public String searchPost(Model model, @RequestParam("search") String searchTitle){
-        List<Post> searchPosts = postService.getSearchPosts(searchTitle);
-        model.addAttribute("searchPosts",searchPosts);
-        return "searchpost";
-    }
 
-
-//    private HttpSession httpSession;
     public User user = new User("na","ia","pa","",""); //임시용
     @RequestMapping("/makepost")
-    public String makePost(){
-//        httpSession.getAttribute("user"); //현재 로그인 중인 user가 존재하는가
-        return "makepost";
+    public void makePost(){
+//        ("user"); //현재 로그인 중인 user가 존재하는가
     }
 
-    @RequestMapping("/makepostsubmit")
-    public String makePostSubmit(@RequestParam("title") String title,
+    @PostMapping("/makepostsubmit")
+    public void makePostSubmit(@RequestParam("title") String title,
                                  @RequestParam("content") String content,
                                  @RequestParam("category") String category,
                                  @RequestParam("goodsLink") String goodsLink,
                                  @RequestParam("limitNumberOfPeople") Long limitNumberOfPeople,
                                  @RequestParam("deadline") String deadline){
 
-//        User user = (User)httpSession.getAttribute("user"); //현재 로그인중 user 정보가져오기
+//        ("user"); //현재 로그인중 user 정보가져오기
         postService.savePost(user.getUserId(), title, content, category, goodsLink, limitNumberOfPeople, deadline);
-        return "redirect:/";
     }
 
-    @RequestMapping("/post/{postId}/update")
-    public String updatePost(Model model, @PathVariable("postId") Long postId){
-
-        Post post = postService.getPost(postId).get();
-        model.addAttribute("post", post);
-        return "updatepost";
+    @GetMapping("/post/{postId}/update")
+    public Post updatePost(@PathVariable("postId") Long postId){
+        return postService.getPost(postId).get();
     }
 
-    @RequestMapping("/updatepostsubmit/{postId}")
-    public String updatePostSubmit(@PathVariable("postId") Long postId,
+    @PutMapping("/updatepostsubmit/{postId}")
+    public void updatePostSubmit(@PathVariable("postId") Long postId,
                              @RequestParam("title") String title,
                              @RequestParam("content") String content,
                              @RequestParam("category") String category,
@@ -72,17 +60,14 @@ public class PostController {
                              @RequestParam("limitNumberOfPeople") Long limitNumberOfPeople,
                              @RequestParam("deadline") String deadline){
 
-//        User user = (User)httpSession.getAttribute("user"); //현재 로그인중인 user
+//        ("user"); //현재 로그인중인 user
         postService.updatePost(postId, title,content,category,goodsLink,limitNumberOfPeople,deadline);
-        return "redirect:/post/"+postId;
     }
 
     //1.이 페이지에 댓글하고 다 보일건데.. 로그인 한 user가 참여했으면 보이고 참여안했으면 안보임
-    @RequestMapping("/post/{postId}")
-    public String detailPost(Model model, @PathVariable("postId") Long postId){
-        Post post = postService.getPost(postId).get();
-        model.addAttribute("post",post);
-        return "detailpost";
+    @GetMapping("/post/{postId}")
+    public Post detailPost(@PathVariable("postId") Long postId){
+        return postService.getPost(postId).get();
     }
 
 
