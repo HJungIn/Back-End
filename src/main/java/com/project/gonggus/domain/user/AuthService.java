dd39a7040fa41d3873d242b137cd56c5072211ed
백상수 <skygana@gmail.com>
2021-01-03 15:52:01 +0900

    change user auth logic

---
+++ src/main/java/com/project/gonggus/domain/user/AuthService.java
@@ -0,0 +1,87 @@
+package com.project.gonggus.domain.user;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class AuthService {
+    private final String JWT_SECRET = "testkey";
+    private final Long EXPIRE_DAY = 7L;
+    private final Long ONE_DAY = 86400L;
+
+    // 토큰 생성
+    public String create (final User user) {
+        final JwtBuilder builder = Jwts.builder();
+        builder.setHeaderParam("alg", "HS256")
+                .setHeaderParam("typ", "JWT")
+                .setSubject("userId")
+                .claim("id", user.getId())
+                .claim("userId", user.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * EXPIRE_DAY))
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET.getBytes());
+        final String jwt = builder.compact();
+        return jwt;
+    }
+
+    // 토큰 검증 후 정보 얻기
+    public Map<String, Object> get (final String jwt) {
+        Jws<Claims> claims = null;
+        try {
+            claims = Jwts.parser()
+                        .setSigningKey(JWT_SECRET.getBytes())
+                        .parseClaimsJws(jwt);
+        } catch (final Exception e) {
+            throw new RuntimeException();
+        }
+
+        return claims.getBody();
+    }
+
+    // 비밀번호 SHA-256으로 암호화
+    public String encryptString (String input){
+        try {
+            MessageDigest sha = MessageDigest.getInstance("SHA-256");
+            sha.update(input.getBytes());
+            byte byteData[] = sha.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+        } catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Cookie setAuthCookie (String token) {
+        Cookie cookie = new Cookie("access_token", token);
+        cookie.setMaxAge((int)(EXPIRE_DAY * ONE_DAY));
+        cookie.setHttpOnly(true);
+        return cookie;
+    }
+
+    public Cookie findAuthCookie (Cookie[] cookies) {
+        for (Cookie c : cookies) {
+            if (c.getName().equals("access_token")) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public Cookie removeCookie () {
+        Cookie cookie = new Cookie("access_token", null);
+        cookie.setMaxAge(0);
+        cookie.setHttpOnly(true);
+        return cookie;
+    }
+}
---
+++ src/main/java/com/project/gonggus/domain/user/UserDto.java
@@ -0,0 +1,48 @@
+package com.project.gonggus.domain.user;
+
+import com.project.gonggus.domain.post.Post;
+import com.project.gonggus.domain.userpost.UserPost;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String userId;
+    private String nickname;
+    private String schoolName;
+    private List<Long> participatePosts;
+    private List<Long> ownPosts;
+    private List<Long> bookmarkPosts;
+
+    public static UserDto convert(User user){
+        List<Long> participate = new ArrayList<>();
+        for(UserPost up: user.getParticipatePosts()) {
+            participate.add(up.getPost().getId());
+        }
+        List<Long> own = new ArrayList<>();
+        for(Post up: user.getOwnPosts()) {
+            own.add(up.getId());
+        }
+
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .userId(user.getUserId())
+                .nickname(user.getNickname())
+                .schoolName(user.getSchoolName())
+                .participatePosts(participate)
+                .ownPosts(own)
+                .bookmarkPosts(user.getBookmarkPosts())
+                .build();
+    }
+
+}
