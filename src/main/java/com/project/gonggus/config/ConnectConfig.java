f7e4eaf5d7aaab5b9e0249bef6dab140cc6efec5
HJungIn <gjwjddls96@naver.com>
2021-01-11 14:02:41 +0900

    https config

---
+++ src/main/java/com/project/gonggus/config/ConnectConfig.java
@@ -0,0 +1,22 @@
+package com.project.gonggus.config;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConnectConfig {
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                context.setCookieProcessor(new LegacyCookieProcessor());
+            }
+        };
+        return tomcat;
+    }
+
+}
