bec987bd1bea767d6e46f131ef93ea22c813606e
HJungIn <gjwjddls96@naver.com>
2021-01-11 13:23:36 +0900

    https config

--- src/main/java/com/project/gonggus/config/ConnectorConfig.java
+++ src/main/java/com/project/gonggus/config/ConnectorConfig.java
@@ -18,25 +18,10 @@ public class ConnectorConfig {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
                 context.setCookieProcessor(new LegacyCookieProcessor());
             }
         };
-//        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
         return tomcat;
     }
 
-//    private Connector getHttpConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(8080);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(443);
-//        return connector;
-//    }
 }
